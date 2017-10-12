0f3255c10b6dd1c5e58e54b7818af8e4b8b73b5e
Hunter Nichols <hnicho11@uncc.edu>
2017-10-12 01:54:47 +0000

    completed 4.7

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,14 +8,22 @@ class MoviesController < ApplicationController
     id = params[:id] 
     @movie = Movie.find(id) 
     byebug
-    logger.debug("I am a debug message")
-    raise params.inspect
+    #logger.debug("I am a debug message")
+    #raise params.inspect
   end
   
   def new
-  @movie = Movie.new
-  # default: render 'new' template
+    @movie = Movie.new
+    # default: render 'new' template
   end 
+  
+  # in movies_controller.rb
+  def create
+    #@movie = Movie.create!(params[:movie]) #old way
+    @movie = Movie.create!(movie_params)  # new way
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
 
   # add below all other methods
 private
