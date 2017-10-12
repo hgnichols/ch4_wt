ca2101fd54d44849afe09a44fa3c6a20b9c5bfef
Hunter Nichols <hnicho11@uncc.edu>
2017-10-12 01:01:24 +0000

    Completed 4.4

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,18 @@
+# This file is app/controllers/movies_controller.rb
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+  
+  def show
+    id = params[:id] 
+    @movie = Movie.find(id) 
+  end
+  
+  # add below all other methods
+private
+
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
+end
